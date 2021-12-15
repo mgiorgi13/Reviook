ebdad3dc207cda8ae2324e6c7ac5ec2800d36e42
mattiadido95 <mattia.didonato95@gmail.com>
2021-12-15 17:45:21 +0100

    fix

--- Reviook_app/src/main/java/it/unipi/dii/reviook_app/HelloApplication.java
+++ Reviook_app/src/main/java/it/unipi/dii/reviook_app/HelloApplication.java
@@ -4,7 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -15,6 +23,32 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+//        VBox layout = new VBox();
+//        VBox layout2 = new VBox();
+//        layout.setAlignment(Pos.CENTER);
+//        layout2.setAlignment(Pos.CENTER);
+//
+//        Scene scene = new Scene(layout, 300, 300);
+//        Scene scene2 = new Scene(layout2, 300, 300);
+//
+//        Label label1 = new Label("This is the First Scene");
+//        Label label2 = new Label("This is the Second Scene");
+//
+//        Button button = new Button("Forward");
+//        button.setOnAction(e -> stage.setScene(scene2));
+//
+//        Button button2 = new Button("Backwards");
+//        button2.setOnAction(e -> stage.setScene(scene));
+//
+//        TextField text = new TextField();
+//        text.setMaxWidth(100);
+//
+//        layout.getChildren().addAll(label1, button);
+//        layout2.getChildren().addAll(label2, button2, text);
+//
+//        stage.setTitle("CodersLegacy");
+//        stage.setScene(scene);
+//        stage.show();
     }
 
     public static void main(String[] args) {
