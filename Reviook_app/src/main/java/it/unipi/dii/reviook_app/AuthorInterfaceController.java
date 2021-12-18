86ca6b6899bb4558ed5cd633314dbddaa253dcd5
mattiadido95 <mattia.didonato95@gmail.com>
2021-12-18 16:03:53 +0100

    page author and user init

---
+++ Reviook_app/src/main/java/it/unipi/dii/reviook_app/AuthorInterfaceController.java
@@ -0,0 +1,37 @@
+package it.unipi.dii.reviook_app;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class AuthorInterfaceController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    @FXML
+    void changeAuthor(ActionEvent event) throws IOException{
+        Parent userInterface = FXMLLoader.load(getClass().getResource("user-interface.fxml"));
+        Stage actual_stage = (Stage) button.getScene().getWindow();
+        actual_stage.setScene(new Scene(userInterface));
+        actual_stage.setResizable(false);
+        actual_stage.show();
+    }
+}
