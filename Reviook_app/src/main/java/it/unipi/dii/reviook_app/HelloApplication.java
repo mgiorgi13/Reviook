756067605571ca95b94d56bff382a76f5a50995f
mattiadido95 <mattia.didonato95@gmail.com>
2021-12-18 16:52:25 +0100

    fix

--- Reviook_app/src/main/java/it/unipi/dii/reviook_app/HelloApplication.java
+++ Reviook_app/src/main/java/it/unipi/dii/reviook_app/HelloApplication.java
@@ -4,15 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.geometry.Pos;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+
 import java.io.IOException;
 import javafx.scene.Parent;
 
