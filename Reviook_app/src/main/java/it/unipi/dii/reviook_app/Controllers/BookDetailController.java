0906e683bfe4c9a809143bc39b5b6d5665dff0c9
mattiadido95 <mattia.didonato95@gmail.com>
2022-01-03 15:04:14 +0100

    implementata lista custom in book detail per le review

--- Reviook_app/src/main/java/it/unipi/dii/reviook_app/Controllers/BookDetailController.java
+++ Reviook_app/src/main/java/it/unipi/dii/reviook_app/Controllers/BookDetailController.java
@@ -1,16 +1,26 @@
 package it.unipi.dii.reviook_app.Controllers;
 
 import com.jfoenix.controls.JFXButton;
+import it.unipi.dii.reviook_app.Components.ListElem;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.util.Callback;
+
 
 public class BookDetailController {
     @FXML
@@ -23,6 +33,28 @@ public class BookDetailController {
     private JFXButton searchButton;
 
     @FXML
+    private ListView listView;
+
+    private List<String> stringList = new ArrayList<>(5);
+
+    private ObservableList observableList = FXCollections.observableArrayList();
+
+    public void setListView() {
+        for (int i = 0; i < 10; i++) {
+            stringList.add("");
+        }
+        observableList.setAll(stringList);
+        listView.setItems(observableList);
+        listView.setCellFactory(
+                new Callback<ListView<String>, javafx.scene.control.ListCell<String>>() {
+                    @Override
+                    public ListCell<String> call(ListView<String> listView) {
+                        return new ListElem();
+                    }
+                });
+    }
+
+    @FXML
     void searchInterface(ActionEvent event) throws IOException {
         Parent searchInterface = FXMLLoader.load(getClass().getResource("/it/unipi/dii/reviook_app/fxml/search.fxml"));
         Stage actual_stage = (Stage) searchButton.getScene().getWindow();
@@ -33,7 +65,7 @@ public class BookDetailController {
 
     @FXML
     void initialize() {
-
+        setListView();
     }
 }
 
