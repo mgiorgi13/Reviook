9d96093937baa419877a607a918728ec9c0ad470
mgiorgi13 <96mat96@libero.it>
2022-01-27 19:32:14 +0100

    fixed login call

--- Reviook_app/src/main/java/it/unipi/dii/reviook_app/controllers/LoginController.java
+++ Reviook_app/src/main/java/it/unipi/dii/reviook_app/controllers/LoginController.java
@@ -97,19 +97,27 @@ public class LoginController {
             user_scene = FXMLLoader.load(getClass().getResource("/it/unipi/dii/reviook_app/fxml/admin.fxml"));
         } else {
             if (session.getIsAuthor()) {
-                user_scene = FXMLLoader.load(getClass().getResource("/it/unipi/dii/reviook_app/fxml/author.fxml"));
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/it/unipi/dii/reviook_app/fxml/user.fxml"));
+                user_scene = (Parent) fxmlLoader.load();
+                AuthorInterfaceController authorInterfaceController = fxmlLoader.getController();
+                authorInterfaceController.setAuthor(session.getLoggedAuthor());
             } else {
-                user_scene = FXMLLoader.load(getClass().getResource("/it/unipi/dii/reviook_app/fxml/user.fxml"));
-                List<String> Follow = userManager.loadRelations("User", username);
-                session.getLoggedUser().getInteractions().setNumberFollow(Follow.size());
-                for (int i = 0; i < Follow.size(); i++) {
-                    session.getLoggedUser().getInteractions().setFollower(Follow.get(i));
-                }
-                List<String> Follower = userManager.loadRelationsFollower("User", username);
-                session.getLoggedUser().getInteractions().setNumberFollow(Follower.size());
-                for (int i = 0; i < Follower.size(); i++) {
-                    session.getLoggedUser().getInteractions().setFollower(Follower.get(i));
-                }
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/it/unipi/dii/reviook_app/fxml/user.fxml"));
+                user_scene = (Parent) fxmlLoader.load();
+                UserInterfaceController userInterfaceController = fxmlLoader.getController();
+                userInterfaceController.setUser(session.getLoggedUser());
+
+
+//                List<String> Follow = userManager.loadRelations("User", username);
+//                session.getLoggedUser().getInteractions().setNumberFollow(Follow.size());
+//                for (int i = 0; i < Follow.size(); i++) {
+//                    session.getLoggedUser().getInteractions().setFollower(Follow.get(i));
+//                }
+//                List<String> Follower = userManager.loadRelationsFollower("User", username);
+//                session.getLoggedUser().getInteractions().setNumberFollow(Follower.size());
+//                for (int i = 0; i < Follower.size(); i++) {
+//                    session.getLoggedUser().getInteractions().setFollower(Follower.get(i));
+//                }
             }
         }
         // TODO commentato perche inutile
