4bea60958c7b228013510c58df73af82005343ee
mgiorgi13 <96mat96@libero.it>
2022-02-05 13:02:27 +0100

    changes to insert folllower relationship counter  in mongo

--- Reviook_app/src/main/java/it/unipi/dii/reviook_app/Reviook.java
+++ Reviook_app/src/main/java/it/unipi/dii/reviook_app/Reviook.java
@@ -1,32 +1,32 @@
-package it.unipi.dii.reviook_app;
-
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-import java.io.IOException;
-import java.text.ParseException;
-
-@SuppressWarnings("unchecked")
-public class Reviook extends Application {
-
-    @Override
-    public void start(Stage stage) throws IOException, ParseException {
-        Parent root = FXMLLoader.load(getClass().getResource("/it/unipi/dii/reviook_app/fxml/login.fxml"));
-        stage.setTitle("Reviook");
-        stage.setResizable(false);
-        stage.getIcons().add(new Image("/book.jpg"));
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
-
-//    public static void main(String[] args) {
-//        launch();
+//package it.unipi.dii.reviook_app;
 //
+//
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.scene.image.Image;
+//import javafx.stage.Stage;
+//import java.io.IOException;
+//import java.text.ParseException;
+//
+//@SuppressWarnings("unchecked")
+//public class Reviook extends Application {
+//
+//    @Override
+//    public void start(Stage stage) throws IOException, ParseException {
+//        Parent root = FXMLLoader.load(getClass().getResource("/it/unipi/dii/reviook_app/fxml/login.fxml"));
+//        stage.setTitle("Reviook");
+//        stage.setResizable(false);
+//        stage.getIcons().add(new Image("/book.jpg"));
+//        stage.setScene(new Scene(root));
+//        stage.show();
 //    }
-
-}
-
+//
+////    public static void main(String[] args) {
+////        launch();
+////
+////    }
+//
+//}
+//
